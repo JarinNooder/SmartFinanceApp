b6a647f64f3f111bc916957c7626b5d122f13047
JarinNooder <estyjarin499@gmail.com>
2026-03-22 18:23:54 -0600

    Add button click functionality with Toast

--- app/src/main/java/com/example/smartfinanceapp/MainActivity.kt
+++ app/src/main/java/com/example/smartfinanceapp/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.smartfinanceapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnAdd = findViewById<Button>(R.id.btnAddTransaction)
+        val btnHistory = findViewById<Button>(R.id.btnViewHistory)
+
+        btnAdd.setOnClickListener {
+            Toast.makeText(this, "Add Transaction Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        btnHistory.setOnClickListener {
+            Toast.makeText(this, "View History Clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
